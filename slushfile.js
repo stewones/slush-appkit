180fa64899b3fe60d21c56a998fd304a4751491f
stewones <talk@stpa.co>
2016-05-27 12:02:48 -0300

    feat(component/cli): adding template

--- slushfile.js
+++ slushfile.js
@@ -76,12 +76,14 @@ gulp.task('client-module', function (done) {
 });
 
 //create angular controller
-gulp.task('client-controller', function (done) {
+gulp.task('client-controller', ['client-controller-generator']);
+gulp.task('client-ctrl', ['client-controller-generator']);
+gulp.task('client-controller-generator', function (done) {
     var args = this.args;
     if (!this.args[0] || !this.args[1]) {
         console.log('###################################################################################'.yellow);
         console.log('######   Incorrect usage                                                     ######'.yellow);
-        console.log('######   Try slush appkit:client-controller <moduleName> <controllerName>  ######'.yellow);
+        console.log('######   Try slush appkit:client-controller <moduleName> <controllerName>    ######'.yellow);
         console.log('######   Example: `slush appkit:client-controller user list`                 ######'.yellow);
         console.log('###################################################################################'.yellow);
         return done();
@@ -89,25 +91,71 @@ gulp.task('client-controller', function (done) {
     inquirer.prompt([
         //{ type: 'input', name: 'module', message: 'What is the module?', default: gulp.args.join(' ') },
         //{ type: 'input', name: 'name', message: 'What is the name of controller?', default: gulp.args.join(' ') },
-        { type: 'confirm', name: 'moveon', message: 'Continue?' }
+        // { type: 'confirm', name: 'moveon', message: 'Continue?' }
     ],
         function (answers) {
-            if (!answers.moveon) {
-                return done();
-            }
+            // if (!answers.moveon) {
+            //     return done();
+            // }
             answers.moduleName = args[0];
             answers.controllerName = args[1];
 
-            var inject = [__dirname + '/template/angular/controller.js'];
-            gulp.src(inject) // Note use of __dirname to be relative to generator              
-                .pipe(template(answers))                 // Lodash template support
-
-                .pipe(conflict('./client/src/app/modules/' + answers.moduleName))                    // Confirms overwrites on file conflicts
-                .pipe(rename(answers.controllerName + '.controller.js'))
-                .pipe(gulp.dest('./client/src/app/modules/' + answers.moduleName))                   // Without __dirname here = relative to cwd
-                .pipe(install())                         // Run `bower install` and/or `npm install` if necessary
+            var inject = [__dirname + '/template/angular/controller/**']; // Note use of __dirname to be relative to generator
+            gulp.src(inject)
+                .pipe(template(answers)) // Lodash template support               
+                .pipe(rename(function (path) { //rename files
+                    if (path.extname) {
+                        path.basename = answers.controllerName + '.' + path.basename;
+                    }
+                }))
+                .pipe(conflict('./client/src/app/modules/' + answers.moduleName)) // Confirms overwrites on file conflicts
+                .pipe(gulp.dest('./client/src/app/modules/' + answers.moduleName)) // Without __dirname here = relative to cwd
+                .pipe(install()) // Run `bower install` and/or `npm install` if necessary
                 .on('end', function () {
-                    done();                              // Finished!
+                    done(); // Finished!
+                })
+                .resume();
+        });
+});
+
+//create angular component
+gulp.task('client-comp', ['client-component-generator']);
+gulp.task('client-component', ['client-component-generator']);
+gulp.task('client-component-generator', function (done) {
+    var args = this.args;
+    if (!this.args[0] || !this.args[1]) {
+        console.log('#################################################################################'.yellow);
+        console.log('######   Incorrect usage                                                   ######'.yellow);
+        console.log('######   Try slush appkit:client-component <moduleName> <componentName>    ######'.yellow);
+        console.log('######   Example: `slush appkit:client-component user list`                ######'.yellow);
+        console.log('#################################################################################'.yellow);
+        return done();
+    }
+    inquirer.prompt([
+        //{ type: 'input', name: 'module', message: 'What is the module?', default: gulp.args.join(' ') },
+        //{ type: 'input', name: 'name', message: 'What is the name of controller?', default: gulp.args.join(' ') },
+        // { type: 'confirm', name: 'moveon', message: 'Continue?' }
+    ],
+        function (answers) {
+            // if (!answers.moveon) {
+            //     return done();
+            // }
+            answers.moduleName = args[0];
+            answers.componentName = args[1];
+
+            var inject = [__dirname + '/template/angular/component/**']; // Note use of __dirname to be relative to generator
+            gulp.src(inject)
+                .pipe(template(answers)) // Lodash template support               
+                .pipe(rename(function (path) { //rename files
+                    if (path.extname) {
+                        path.basename = answers.componentName + '.' + path.basename;
+                    }
+                }))
+                .pipe(conflict('./client/src/app/modules/' + answers.moduleName + '/components/' + answers.componentName)) // Confirms overwrites on file conflicts
+                .pipe(gulp.dest('./client/src/app/modules/' + answers.moduleName + '/components/' + answers.componentName)) // Without __dirname here = relative to cwd
+                .pipe(install()) // Run `bower install` and/or `npm install` if necessary
+                .on('end', function () {
+                    done(); // Finished!
                 })
                 .resume();
         });
@@ -124,7 +172,6 @@ gulp.task('serve-client', function (done) {
         }));
 });
 
-
 //serve api
 gulp.task('serve-api', function () {
     // configure nodemon
@@ -138,7 +185,6 @@ gulp.task('serve-api', function () {
         gutil.log('Modified', gutil.colors.yellow(log))
     });
 });
-
 
 //serve all
 gulp.task('serve', function () {
