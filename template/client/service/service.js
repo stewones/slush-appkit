ccac92a4007989502f0fba79f87cd0edc6236db8
stewones <talk@stpa.co>
2016-06-04 10:10:50 -0300

    feat(service/template): adds the service cli option

---
+++ template/client/service/service.js
@@ -0,0 +1,19 @@
+(function () {
+    'use strict';
+    /**
+     * @ngdoc object
+     * @name app.<%= moduleName %>.module.service:<%= serviceNameStartCase %>
+     **/
+    angular.module('<%= moduleName %>.module').service('<%= serviceNameStartCase %>', /*@ngInject*/ function (lodash) {
+        var _ = lodash;
+        var <%= serviceNameStartCase %> = function (params) {
+            params = params ? params : {};
+            angular.extend(this, params);
+        }
+        <%= serviceNameStartCase %>.prototype.method = method;
+        function method() {
+            //code goes here    
+        }
+        return <%= serviceNameStartCase %>;
+    });
+});
